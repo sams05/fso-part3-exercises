3005e3cfbc46b8f06ed84a0a1d58765a1aa63fdb
Samuel Shen <sshen05@terpmail.umd.edu>
2024-02-19 12:27:28 -0500

    Finish ex3.9

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
+const cors = require('cors');
 
 let persons = [
     {
@@ -33,6 +34,7 @@ morgan.token('req-data', (req) => {
     }
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req-data')); // Must be used after the json parser for easy access to request data
 
@@ -52,9 +54,9 @@ app.get('/api/persons/:id', (request, response) => {
     const person = persons.find((person) => person.id === id);
     if (person) {
         response.json(person);
+    } else {
+        response.status(404).end();
     }
-
-    response.status(404).end();
 });
 
 app.delete('/api/persons/:id', (request, response) => {
