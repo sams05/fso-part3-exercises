4bdb23554a201b3dad4b8a6772c61082b09b5a52
Samuel Shen <sshen05@terpmail.umd.edu>
2024-02-21 22:22:32 -0500

    Finish ex3.13

--- index.js
+++ index.js
@@ -1,30 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
 const cors = require('cors');
-
-let persons = [
-    {
-        id: 1,
-        name: 'Arto Hellas',
-        number: '040-123456',
-    },
-    {
-        id: 2,
-        name: 'Ada Lovelace',
-        number: '39-44-5323523',
-    },
-    {
-        id: 3,
-        name: 'Dan Abramov',
-        number: '12-43-234345',
-    },
-    {
-        id: 4,
-        name: 'Mary Poppendieck',
-        number: '39-23-6423122',
-    },
-];
+const Person = require('./models/person');
 
 morgan.token('req-data', (req) => {
     if (req.method === 'POST') {
@@ -47,17 +26,20 @@ app.get('/info', (request, response) => {
 });
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons);
+    Person.find({}).then(persons => {
+        response.json(persons);
+    });
 });
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = +request.params.id;
-    const person = persons.find((person) => person.id === id);
-    if (person) {
-        response.json(person);
-    } else {
-        response.status(404).end();
-    }
+    const id = request.params.id;
+    Person.findById(id).then(person => {
+        if(person) {
+            response.json(person)
+        } else {
+            response.status(404).end();
+        }
+    })
 });
 
 app.delete('/api/persons/:id', (request, response) => {
--- mongo.js
+++ mongo.js
@@ -21,7 +21,35 @@ const personSchema = new mongoose.Schema({
 });
 
 const Person = mongoose.model('Person', personSchema);
+/*
+let persons = [
+    {
+        id: 1,
+        name: 'Arto Hellas',
+        number: '040-123456',
+    },
+    {
+        id: 2,
+        name: 'Ada Lovelace',
+        number: '39-44-5323523',
+    },
+    {
+        id: 3,
+        name: 'Dan Abramov',
+        number: '12-43-234345',
+    },
+    {
+        id: 4,
+        name: 'Mary Poppendieck',
+        number: '39-23-6423122',
+    },
+];
 
+persons.forEach(person => {
+    const personDoc = new Person(person);
+    personDoc.save().catch(error => console.error('fail to save', person));
+});
+*/
 if (process.argv.length === 3) {
     Person.find({}).then((people) => {
         console.log('phonebook:');
